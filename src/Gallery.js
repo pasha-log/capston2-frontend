3905462ccf94a00e415461ad89076bf07cf0f042
pasha-log <pasha.log@gmail.com>
2023-03-28 14:23:45 -0700

    created separate component for the gallery

---
+++ src/Gallery.js
@@ -0,0 +1,42 @@
+import './Profile.css';
+import { Link } from 'react-router-dom';
+
+const Gallery = ({ userBeingViewed }) => {
+	return (
+		<div className="gallery">
+			{userBeingViewed?.posts?.slice(0).reverse().map((post) => {
+				return (
+					<Link
+						to={`/posts/${post.postId}`}
+						key={post.postId}
+						state={{
+							imageURL: post.postURL,
+							caption: post.caption,
+							username: userBeingViewed?.username,
+							profileImageURL: userBeingViewed?.profileImageURL,
+							postId: post.postId
+						}}
+					>
+						<div className="gallery-item" tabIndex="0">
+							<img className="gallery-image" src={post.postURL} alt={post.id} />
+							<div className="gallery-item-info">
+								<ul>
+									<li className="gallery-item-likes" key={`${post.postId}-gallery-item-likes`}>
+										<span className="material-symbols-outlined visually-hidden">favorite</span>
+										0
+									</li>
+									<li className="gallery-item-comments" key={`${post.postId}-gallery-item-comments`}>
+										<span className="material-symbols-outlined visually-hidden">mode_comment</span>
+										0
+									</li>
+								</ul>
+							</div>
+						</div>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
+
+export default Gallery;
--- src/Profile.js
+++ src/Profile.js
@@ -5,6 +5,7 @@ import { useContext, useState, useEffect } from 'react';
 import CurrentUserContext from './CurrentUserContext';
 import './Profile.css';
 import InstapostApi from './Api';
+import Gallery from './Gallery';
 
 const Profile = () => {
 	const { username } = useParams();
@@ -32,7 +33,6 @@ const Profile = () => {
 	}
 
 	return (
-		// https://codepen.io/GeorgePark/pen/VXrwOP
 		<div>
 			<header>
 				<div className="ProfileContainer">
@@ -104,40 +104,11 @@ const Profile = () => {
 							</Link>
 						</div> :
 						<div className="ProfileContainer">
-							<div className="gallery">
-							{userBeingViewed?.posts?.slice(0).reverse().map(post => {return (
-							<Link to={`/posts/${post.postId}`} key={post.postId} state={{imageURL: post.postURL, 
-																						caption: post.caption, 
-																						username: userBeingViewed?.username, 
-																						profileImageURL: userBeingViewed?.profileImageURL,
-																						postId: post.postId}}>
-								<div className="gallery-item" tabIndex="0">
-										<img className="gallery-image" src={post.postURL} alt={post.id}/>
-										<div className="gallery-item-info">
-										<ul>
-											<li className="gallery-item-likes" key={`${post.postId}-gallery-item-likes`}>
-												<span className="material-symbols-outlined visually-hidden">
-												favorite
-												</span>
-												 0
-											</li>
-											<li className="gallery-item-comments" key={`${post.postId}-gallery-item-comments`}>
-												<span className="material-symbols-outlined visually-hidden">
-												mode_comment
-												</span>
-												 0
-											</li>
-										</ul>
-									</div>
-								</div>
-							</Link>
-							)})}
-							</div>
+							<Gallery userBeingViewed={userBeingViewed} />
 						</div>
 					}
 				</main>
 		</div>
-		// https://codepen.io/GeorgePark/pen/VXrwOP
 	);
 };
 
